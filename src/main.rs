dfe036454f0c80a3ace84374ae6d19a2c22e0ffc
Christopher Moussa <moussa1@llnl.gov>
2020-04-27 13:37:47 -0700

    main.rs: remove debug statements
    remove debug statements and unecessary
    imports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use serde_yaml; // 0.8.7
 extern crate yaml_rust;
 use yaml_rust::{YamlLoader, YamlEmitter};
-// use serde_yaml::Value;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
@@ -31,17 +30,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Multi document support, doc is a yaml::Yaml
     let doc = &docs[0];
-
-    // Debug support
-    println!("{:?}", doc);
-
-    // let basic = std::fs::File::open("basic.yaml")?;
-    // let x: Value = serde_yaml::from_reader(basic)?;
-    // let o_docs = YamlLoader::load_from_str(x).unwrap();
-
-    // Index access for map & array
-    // assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
-    // assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
 
     // Chained key/array access is checked and won't panic,
     // return BadValue if they are not exist.
