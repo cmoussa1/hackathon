9be996ae3f97483528e5b97c58a04644bd78ba9f
Christopher Moussa <moussa1@llnl.gov>
2020-05-01 09:57:06 -0700

    main.rs: improve everything
    - implement proper processing of
    dependencies attribute in YAML file
    - incorporate matching on dependencies
    traits with dependency Enums
    - push dependencies onto vector of
    struct Dependency
    - include YAML file name as command line
    argument

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,100 @@
-use serde_yaml; // 0.8.7
 extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use std::fs::read_to_string;
+use std::fmt::Debug;
+use std::env;
 
-    let f = std::fs::File::open("something.yaml")?;
-    let d: String = serde_yaml::from_reader(f)?;
-    println!("Read YAML string: {}", d);
+use yaml_rust::{YamlLoader};
 
-    let s =
-    "
-    version: 1
-    resources:
-      - type: slot
-        count: 1
-        label: foo
-        with:
-          - type: node
-            count: 1
-    tasks:
-      - command: [ \"app\" ]
-        slot: foo
-        count:
-          per_slot: 1
-    attributes:
-    ";
+#[derive(Debug)]
+enum DependencyType {
+    In,
+    Out,
+    InOut,
+}
 
-    let docs = YamlLoader::load_from_str(s).unwrap();
+#[derive(Debug)]
+enum DependencyScope {
+    User,
+    Global,
+}
 
-    // Multi document support, doc is a yaml::Yaml
-    let doc = &docs[0];
+#[derive(Debug)]
+enum DependencyScheme {
+    String,
+    Fluid,
+}
 
-    // Chained key/array access is checked and won't panic,
-    // return BadValue if they are not exist.
-    assert!(doc["INVALID_KEY"][100].is_badvalue());
+#[derive(Debug)]
+struct Dependency {
+    dep_type: DependencyType,
+    scope: DependencyScope,
+    scheme: DependencyScheme,
+    value: String,
+}
 
-    // Dump the YAML object
-    let mut out_str = String::new();
-    {
-        let mut emitter = YamlEmitter::new(&mut out_str);
-        emitter.dump(doc).unwrap(); // dump the YAML object to a String
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    // pass filename as first argument on command line
+    let filename: &str = &args[1];
+
+    // create vector of dependencies
+    let dependencies: Vec<Dependency> = parse_yaml(filename);
+    println!("{:#?}", dependencies);
+
+}
+
+// iterate through dependencies array in YAML file and grab hash map
+// for dependencies array in YAML file:
+//     for each depdendency hash map:
+//         grab each key-value pair
+//         push values to vector of dependencies
+// return vector
+fn parse_yaml(yaml_file: &str) -> Vec<Dependency> {
+    let yaml_file = read_to_string(yaml_file).expect("Unable to read file");
+    let contents = YamlLoader::load_from_str(&yaml_file).unwrap();
+
+    let mut dependencies: Vec<Dependency> = Vec::new();
+
+    let dependencies_hash_map = contents[0]["attributes"]["system"]["dependencies"].as_vec().unwrap();
+
+    for hash_map in dependencies_hash_map {
+        // match type to DependencyType enum
+        let d_type = match hash_map["type"].as_str().unwrap() {
+            "in" => Ok(DependencyType::In),
+            "out" => Ok(DependencyType::Out),
+            "inout" => Ok(DependencyType::InOut),
+            _ => Err("error: the type is something else"),
+        };
+        // match scope to DependencyScope enum
+        let d_scope = match hash_map["scope"].as_str().unwrap() {
+            "user" => Ok(DependencyScope::User),
+            "global" => Ok(DependencyScope::Global),
+            _ => Err("error: the scope is something else"),
+        };
+        // match scope to DependencyScheme enum
+        let d_scheme = match hash_map["scheme"].as_str().unwrap() {
+            "string" => Ok(DependencyScheme::String),
+            "fluid" => Ok(DependencyScheme::Fluid),
+            _ => Err("error: the scheme is something else"),
+        };
+        // assign value as string to dependency
+        let value = hash_map["value"].as_str().unwrap();
+
+        let dependency = Dependency {
+            dep_type: d_type.unwrap(),
+            scope: d_scope.unwrap(),
+            scheme: d_scheme.unwrap(),
+            value: value.to_string(),
+        };
+
+        // push dependency to vector of dependencies
+        dependencies.push(dependency);
     }
-    println!("{}", out_str);
 
-    Ok(())
+    // return vector of dependencies
+    dependencies
+
 }
